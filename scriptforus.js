c0e3e4a9c9463d7f1bca45f9bff8208865ee0e22
Anshulrajkumar <anshulrajkumar777@gmail.com>
2025-07-25 03:01:55 +0530

    phone ui bg

--- scriptforus.js
+++ scriptforus.js
@@ -1,4 +1,5 @@
-   
+  
+const video = document.getElementById("bgvideo")
 if (window.innerWidth <= 600) {
     video.src = "assests/video/bg2.mp4"
     document.getElementById("bgvideo").style.objectFit = "contain";
@@ -15,5 +16,28 @@ if (window.innerWidth <= 600) {
     for (i = 0; i < e.length; i++){
         e[i].style.width = "30%";
         e[i].style.height = "30%";
+    }
+        const a = document.getElementsByClassName("fontbigsize");
+    for (let i = 0; i < a.length; i++) {
+        a[i].style.fontSize = "23px";
+    }
+
+    const b = document.getElementsByClassName("fontsmallsize");
+    for (let i = 0; i < b.length; i++) {
+        b[i].style.fontSize = "12px";
+    }
+
+    const c = document.getElementsByClassName("fontmidsize");
+    for (let i = 0; i < c.length; i++) {
+        c[i].style.fontSize = "11px";
+    }
+    
+    const d = document.getElementsByClassName("button");
+    for (i = 0; i < d.length; i++) {
+        d[i].style.width = "60px";
+        d[i].style.height = "4px";
+        d[i].style.border = "2px white solid";
+        d[i].style.fontSize = "10px";
+        d[i].style.borderRadius = "11%";
     }
 }
